7eca603ec064580cfcf4c8f97cb37ce0d68a7b41
miyachik <atto.mark56@gmail.com>
2017-02-11 06:48:53 +0900

    change role

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -7,5 +7,5 @@ set :ssh_options, keys: [File.expand_path('~/.ssh/inkochan.pem')],
                   forward_agent: false,
                   port: 22,
                   auth_methods: %w(publickey)
-server '13.112.77.159', user: 'ec2-user',
+server '13.112.205.87', user: 'ec2-user',
                     roles: %w(web app db)

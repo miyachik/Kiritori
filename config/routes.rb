b20038cf4609d91fa60269e00bcd556dd4e9c8c5
miyachik <atto.mark56@gmail.com>
2017-02-11 05:48:18 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'ab_tests#index'
-  resource :home, only: %i(index create)
-  resource :ab_tests, only: %i(index new create edit update destroy) do
+  resources :homes
+  resources :ab_tests, only: %i(index new create edit update destroy) do
     member do
       get :rebuild
     end

dc2e5a81da05546cb79c4e65a6b7f6af5e486a6c
EC2 Default User <ec2-user@ip-172-31-17-4.ap-northeast-1.compute.internal>
2017-02-10 19:45:45 +0000

    add deploy config

---
+++ Capfile
@@ -0,0 +1,7 @@
+require 'capistrano/setup'
+require 'capistrano/deploy'
+require 'capistrano/rails'
+require 'capistrano/rbenv'
+require 'capistrano3/unicorn'
+
+Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
---
+++ config/deploy/development.rb
@@ -0,0 +1,10 @@
+set :stage, :development
+set :rails_env, 'development'
+#set :pty, false
+
+set :ssh_options, keys: [File.expand_path('~/.ssh/inkochan.pem')],
+                  forward_agent: false,
+                  port: 22,
+                  auth_methods: %w(publickey)
+server '13.112.77.159', user: 'ec2-user',
+                    roles: %w(web app db)

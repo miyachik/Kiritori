00b9bbffb670a68a91ea2a18210e05545f4b5cd6
miyachik <atto.mark56@gmail.com>
2017-03-26 22:58:03 +0900

    suppot reply

--- app/controllers/concerns/slack_handleable.rb
+++ app/controllers/concerns/slack_handleable.rb
@@ -7,14 +7,14 @@ module SlackHandleable
     start_url = url['start']
     end_url = url['end']
     detail = start_url.match(%r{^https://(.+?).slack.com/archives/(.+?)/p(.+)})
-    channel_name = detail[2]
+    channel_id = detail[2]
     start_timestamp = (detail[3].to_f - 1).fdiv(1000000.0)
     end_timestamp = (end_url.match(%r{^https://(.+?).slack.com/archives/(.+?)/p(.+)})[3].to_f + 1).fdiv(1000000.0)
     client = Slack::Client.new
     users = Hash[client.users_list['members'].map { |m| [m['id'], m['name']] }]
-    channels = Hash[client.channels_list['channels'].map { |channel| [channel['name'], channel['id']] }]
+    channels = Hash[client.channels_list['channels'].map { |channel| [channel['id'], channel['name']] }]
     messages = client.channels_history(
-      channel: channels[channel_name],
+      channel: channel_id,
       oldest: start_timestamp,
       latest: end_timestamp,
       count: 1000
@@ -27,6 +27,11 @@ module SlackHandleable
     messages.reverse_each do |message|
       user_name = users[message['user']]
       raw = message['text']
+      replies = raw.scan(/(<@[A-Z0-9]*>)/)
+      replies.each  do |reply|
+        user_id = reply[0].delete("@<>")
+        raw.gsub!(/#{reply[0]}/,"@#{users[user_id]}")
+      end
       text += "**#{user_name}** \n #{raw}\n"
     end
     text

5ebf28376c8a3c67c5152bf322d16e6828565f1a
miyachik <atto.mark56@gmail.com>
2017-03-29 14:06:33 +0900

    remove ab_test settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.normal_app_host = ['127.0.0.1:80']
-  config.ab_app_host = ['127.0.0.2:80']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   root 'homes#index'
   resources :homes
-  resources :ab_tests, only: %i(index new create edit update destroy) do
-    member do
-      get :rebuild
-      get :enable_haproxy
-    end
-  end
 end
